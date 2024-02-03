946dc76a57bd6dca20c1096790c373848d46287f
Shivam Sharma <shivam.sharma.grkinfo@gmail.com>
2024-02-03 23:55:22 +0530

    Updated home

--- src/sections/account/admin-profile.js
+++ src/sections/account/admin-profile.js
@@ -6,10 +6,12 @@ import {
     CardActions,
     CardContent,
     Divider,
+    Grid,
     Typography
 } from '@mui/material';
 import { getInitials } from '../../utils/get-initials';
 import { format } from 'date-fns';
+import logo from '../../components/profile-img.png';
 
 // const user = {
 //   avatar: '/assets/avatars/avatar-anika-visser.png',
@@ -34,50 +36,56 @@ export const AdminProfile = ({ user }) => {
         }
     };
     return (
-        <Card>
-            <CardContent>
-                <Box
-                    sx={{
-                        alignItems: 'center',
-                        display: 'flex',
-                        flexDirection: 'column'
-                    }}
-                >
-                    <Avatar
-                        src={user?.avatar}
+        <Card sx={{ backgroundColor: '#403e57' }}>
+            <CardContent sx={{ alignItems: 'center', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Grid xs={12} md={6} sx={{width: '50%'}}>
+                    <Box
                         sx={{
-                            height: 80,
-                            mb: 2,
-                            width: 80
+                            alignItems: 'center',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            width: '50%'
                         }}
                     >
-                        {getInitials(user?.name)}
-                    </Avatar>
-                    <Typography
-                        gutterBottom
-                        variant="h5"
-                    >
-                        {user?.name}
-                    </Typography>
-                    <Typography
-                        color="text.secondary"
-                        variant="body2"
-                    >
-                        {user?.email}
-                    </Typography>
-                    {/* <Typography
+                        <Avatar
+                            src={user?.avatar}
+                            sx={{
+                                height: 100,
+                                mb: 2,
+                                width: 100
+                            }}
+                        >
+                            {getInitials(user?.name)}
+                        </Avatar>
+                        <Typography
+                            gutterBottom
+                            variant="h3"
+                        >
+                            {user?.name}
+                        </Typography>
+                        <Typography
+                            color="text.secondary"
+                            variant="h6"
+                        >
+                            {user?.email}
+                        </Typography>
+                        {/* <Typography
                         color="text.secondary"
                         variant="body2"
                     >
                         {formatDate(user?.date)}
                     </Typography> */}
-                    {/* <Typography
+                        {/* <Typography
                         color="text.primary"
                         variant="body2"
                     >
                         Available Balance : {user?.coins}
                     </Typography> */}
-                </Box>
+                    </Box>
+                </Grid>
+                <Grid xs={12} md={6} sx={{width: '50%'}}>
+                    <img src={logo} alt='profile' style={{maxWidth: '100%', height: 'auto'}} />
+                </Grid>
             </CardContent>
             <Divider />
             {/* <CardActions>
